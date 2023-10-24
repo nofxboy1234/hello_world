17fa222f49c1c9901dbf69ed1767e792cb288e6a
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-24 15:10:54 +0200

    Test using path helper vs string url with query string parameters

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,4 +1,11 @@
 class HelloWorldController < ApplicationController
   def index
+    @user = User.new(first_name: 'Zoey', last_name: 'Palmboom', id: 1)
+  end
+
+  def greet
+    binding.pry
+    @first_name = params[:first_name]
+    @dog = params[:dog]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   get 'hello_world/index'
+  get 'hello_world/:first_name/greet', to: 'hello_world#greet', as: :greet
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
