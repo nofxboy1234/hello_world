3f0839f036294ceeddb5ffd0e6e782caba19a14b
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-24 12:51:04 +0200

    Use config.before block to use :selenium_chrome_headless

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'Hello world', type: :system do
   describe 'index page' do
     it 'shows the right content' do
       visit hello_world_index_path
+      # sleep(5)
       expect(page).to have_content('Hello, world!')
-      sleep(5)
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,6 +60,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:each, type: :system) do
+    # driven_by :selenium_chrome_headless
+    driven_by :selenium_chrome
+  end
 end
 
 # Capybara.default_driver = :selenium_chrome
