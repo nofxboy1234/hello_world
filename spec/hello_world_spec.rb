323a8d042ffceb9ccd83487978ae1f78e5680631
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-24 12:21:09 +0200

    Add sleep to test to see what is happening in chrome

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Hello world', type: :system do
     it 'shows the right content' do
       visit hello_world_index_path
       expect(page).to have_content('Hello, world!')
+      sleep(5)
     end
   end
 end
